8e59dac364cb70a61f8e1276eb95a3ebbfc1e58e
Wafick <wafickachiambi@gmail.com>
2025-08-10 01:09:05 -0400

    Add weights init

--- Stack/test.py
+++ Stack/test.py
@@ -1,7 +1,7 @@
 import robosuite as suite
 from robosuite import load_composite_controller_config
 from robosuite.wrappers import GymWrapper
-from gymnasium.wrappers import Autoreset
+from gymnasium.wrappers import TransformReward,ClipReward
 import torch
 from torch.distributions import Normal
 from torch import Tensor
@@ -14,17 +14,17 @@ logging.disable(logging.CRITICAL)
 class Actor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(74,256)
+        self.l1 = nn.Linear(66,256)
         self.l2 = nn.Linear(256,256)
-        self.l3 = nn.Linear(256,256)
-        self.lmean = nn.Linear(256,9)
-        self.lstd = nn.Linear(256,9)
+        #self.l3 = nn.Linear(256,256)
+        self.lmean = nn.Linear(256,7)
+        self.lstd = nn.Linear(256,7)
         #self.optim = torch.optim.Adam(self.parameters(),hypers.lr)
 
     def forward(self,obs:Tensor):
         x = F.relu(self.l1(obs))
         x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
+        #x = F.relu(self.l3(x))
         mean = self.lmean(x)
         std = self.lstd(x).clamp(-20,2).exp()
         dist = Normal(mean,std) 
@@ -44,32 +44,34 @@ cont_config = load_composite_controller_config(robot="Panda")
 env_configs = {
     "robots":["Panda"],
     "controller_configs": cont_config,
-    "gripper_types":["JacoThreeFingerDexterousGripper"],
+    #"gripper_types":["JacoThreeFingerDexterousGripper"],
     "has_renderer":True,
     "use_camera_obs":False,
     "has_offscreen_renderer":False,
     "reward_shaping":True,               
     "horizon":500,                        
     "control_freq":20,
-    "reward_scale":2.0
+    "reward_scale":1.0
 }
 def make_env():
     x = suite.make(env_name ="Stack" ,**env_configs)
     x = GymWrapper(x,keys=list(x.active_observables))
     x.metadata = {"render_mode":[]}
-    x = Autoreset(x)
     return x
 
 actor = Actor()
-chk = torch.load("./model_60.pth",map_location="cpu")
+chk = torch.load("./model_10.pth",map_location="cpu")
 actor.load_state_dict(chk["actor state"],strict=True)
 env = make_env()
 
 state,info = env.reset()
-for n in range(10_000):
+for n in range(500):
     st = torch.from_numpy(state).to(torch.float32) 
     _,_,action = actor(st) 
     state,reward,done,info,trunc = env.step(action.detach().tolist())
+    print(reward)
     if done:
         state,info = env.reset()
     env.render()
+
+ 
