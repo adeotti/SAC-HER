8e882843db7464f418bebbe02bc12fcadd8e6312
Wafick <wafickachiambi@gmail.com>
2025-09-17 18:04:01 -0400

    Update gym test file to match the training file

--- gym_env/test.py
+++ gym_env/test.py
@@ -1,30 +1,18 @@
 import gymnasium as gym
-import gymnasium_robotics,torch,random
+import gymnasium_robotics,torch,sys
 gym.register_envs(gymnasium_robotics)
 import numpy as np
 import torch.nn as nn
 from torch.distributions import Normal
 from torch.optim import Adam
 import torch.nn.functional as F
-from gymnasium.wrappers import Autoreset
-from gymnasium.spaces import Box,Dict
 
 class custom(gym.Wrapper):
     def __init__(self,env):
         super().__init__(env)
-        self.observation_space = Dict(
-            {
-            "observation" : Box(-np.inf,np.inf,(9,),np.float64),
-            "achieved_goal" : Box(-np.inf,np.inf,(3,),np.float64),
-            "desired_goal" : Box(-np.inf,np.inf,(3,),np.float64)
-            }
-        )
     
     def reset(self,**kwargs):
-        obs,info = super().reset(**kwargs)
-        target = random.choice([True,True,False])
-        self.env.unwrapped.unwrapped.target_in_the_air = target
-        obs["observation"] = obs["observation"][:9]
+        obs,info = super().reset(**kwargs) 
         self.env.unwrapped.data.qpos[0] = .3  
         self.env.unwrapped.data.qpos[1] = .5  
         self.env.unwrapped.data.qpos[17] = .4
@@ -32,7 +20,6 @@ class custom(gym.Wrapper):
 
     def step(self,action):
         state,reward,done,trunc,info = super().step(action)
-        state["observation"] = state["observation"][:9]
         return state,reward,done,trunc,info
 
 def process_obs(obs:dict):
@@ -42,14 +29,14 @@ def process_obs(obs:dict):
     return torch.from_numpy(np.append(observation,(achieved_goal,desired_goal))).to(dtype=torch.float32)
 
 def make_env():
-    x = gym.make("FetchPickAndPlace-v3",max_episode_steps=50,render_mode="human")
+    x = gym.make("FetchPickAndPlace-v3",max_episode_steps=100,render_mode="human")
     x = custom(x)
     return x
  
 class policy(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(15,64)
+        self.l1 = nn.Linear(31,64)
         self.l2 = nn.Linear(64,64)
         self.mean = nn.Linear(64,4)
         self.std = nn.Linear(64,4)
@@ -69,10 +56,13 @@ class policy(nn.Module):
         return action,log,mean
     
 policyy = policy()
-policyy.load_state_dict(torch.load("./model-8.pth",map_location="cpu"))
+policyy.load_state_dict(torch.load("./model-final.pth",map_location="cpu")["policy_state"])
 env = make_env()
-state = process_obs(env.reset()[0])
-for n in range(1000):
-    action = policyy(state)[0]
-    st,re,done,trunc,info = env.step(action.detach().numpy()) # env.action_space.sample()
-    env.render()
+state = env.reset()[0] 
+for n in range(10000):
+    _,_,action = policyy(process_obs(state))
+    st,re,done,trunc,info = env.step(action.detach().numpy()) 
+    if trunc:
+        state = env.reset()[0] 
+    state = st
+    
