1904b7d574434aea3b6d20f1f4553418400e05c2
Wafick <wafickachiambi@gmail.com>
2025-09-20 22:28:02 -0400

    Update...

--- gym_env/test.py
+++ gym_env/test.py
@@ -26,7 +26,9 @@ def process_obs(obs:dict):
     observation = obs.get("observation")
     achieved_goal = obs.get("achieved_goal")
     desired_goal = obs.get("desired_goal")
-    return torch.from_numpy(np.append(observation,(achieved_goal,desired_goal))).to(dtype=torch.float32)
+    output = torch.from_numpy(np.append(observation,(achieved_goal,desired_goal))).to(dtype=torch.float32)
+    output = (output - output.mean())/(output.std()+ 1e-6).clip(-5,5)
+    return output
 
 def make_env():
     x = gym.make("FetchPickAndPlace-v3",max_episode_steps=100,render_mode="human")
@@ -38,6 +40,7 @@ class policy(nn.Module):
         super().__init__()
         self.l1 = nn.Linear(31,256)
         self.l2 = nn.Linear(256,256)
+        self.l3 = nn.Linear(256,256)
         self.mean = nn.Linear(256,4)
         self.std = nn.Linear(256,4)
         self.optim = Adam(self.parameters(),lr=3e-4)
@@ -45,6 +48,7 @@ class policy(nn.Module):
     def forward(self,obs):
         x = F.relu(self.l1(obs))
         x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
         mean = self.mean(x)
         std = self.std(x).clamp(-20,2).exp()
         dist = Normal(mean,std)
@@ -56,12 +60,12 @@ class policy(nn.Module):
         return action,log,mean
     
 policyy = policy()
-policyy.load_state_dict(torch.load("./model-final.pth",map_location="cpu")["policy_state"])
+policyy.load_state_dict(torch.load("./model-9.pth",map_location="cpu")["policy_state"])
 env = make_env()
 state = env.reset()[0] 
 for n in range(10000):
     _,_,action = policyy(process_obs(state))
-    st,re,done,trunc,info = env.step(action.detach().numpy())
+    st,re,done,trunc,info = env.step(env.action_space.sample())
     if trunc:
         state = env.reset()[0] 
     state = st
