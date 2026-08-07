b67b5b26af3779cfa1d5dc9f5bdc8bc5ca603300
adeotti <wafickachiambi@gmail.com>
2026-08-06 20:18:28 -0400

    add line to load controller and running statistics for the normalization

--- robosuite_env/test.py
+++ robosuite_env/test.py
@@ -1,14 +1,20 @@
-import torch
-import robosuite as suite
+import warnings,logging
+warnings.filterwarnings("ignore")
+logging.disable(logging.CRITICAL)
+
+import torch,sys,robosuite
 import numpy as np
 import gymnasium as gym
 
-from main import Actor,env_configs
+from main import Actor
 from robosuite.wrappers.gym_wrapper import GymWrapper
-from stable_baselines3.common.running_mean_std import RunningMeanStd
+from gymnasium.wrappers.stateful_observation import NormalizeObservation
+from robosuite import load_composite_controller_config
 
-env = suite.make(
-    env_name="Stack", 
+controller = load_composite_controller_config(robot="Panda")
+env = robosuite.make(
+    controller_configs = controller,
+    env_name="Lift", 
     robots="Panda",  
     gripper_types="JacoThreeFingerDexterousGripper",
     has_renderer=True,
@@ -18,29 +24,22 @@ env = suite.make(
     control_freq = 20
 )
 env = GymWrapper(env,list(env.observation_spec()))
-
-def normalize(obs,obs_rms:RunningMeanStd): # norm obs
-    mean = torch.from_numpy(obs_rms.mean)
-    std = torch.from_numpy(obs_rms.var).sqrt() 
-    output = (obs - mean) / (std + 1e-8)
-    return output.clamp(-5,5).to(dtype=torch.float32)
+env = NormalizeObservation(env)
 
 obs = env.reset()[0]
 policy = Actor()
 
-checkpoint = torch.load("./19.pth",map_location="cpu",weights_only=False) 
+checkpoint = torch.load("./59.pth",map_location="cpu",weights_only=False) 
 policy.load_state_dict(checkpoint["actor state"])
 
-obs_rms = RunningMeanStd(shape=(162,))
-obs_rms.mean = checkpoint["obs_rms_mean"]
-obs_rms.var = checkpoint["obs_rms_var"]
-obs_rms.count = checkpoint["obs_rms_count"]
+env.obs_rms.mean = checkpoint["obs_rms_mean"].numpy()
+env.obs_rms.var = checkpoint["obs_rms_var"].numpy()
+env.update_running_mean = True 
 
-with torch.no_grad():
-    for i in range(100000):
-        _,_,action = policy(normalize(torch.from_numpy(obs),obs_rms))
-        obs,reward,done,info,trunc = env.step(action.numpy())
-        env.render()
-        if trunc or done:
-            obs = env.reset()[0]
-
+for i in range(100000):
+    _,_,action = policy(torch.from_numpy(obs).float())
+    obs,reward,done,info,trunc = env.step(action.detach().numpy())
+    env.render()
+    sys.exit(env.obs_rms.mean)
+    if trunc or done:
+        obs = env.reset()[0]
